6707c901dbee85467cb35135e085725166a8deb7
user <user@users-Mac.local>
2024-02-23 15:30:04 -0800

    initial commit

---
+++ Assign2-Group3/Assign2_Group3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assign2_Group3App.swift
+//  Assign2-Group3
+//
+//  Created by user on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Assign2_Group3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assign2-Group3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assign2-Group3
+//
+//  Created by user on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
